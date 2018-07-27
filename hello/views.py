0a43f3ee675199c7432538b701d65ff37ace5a36
alvezgr <alvezpope@gmail.com>
2018-07-27 18:50:03 -0300

    New route to pokemon in urls.py and pokemon function in views.py

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .poke import get_pokemons
+from .pokecard import get_card
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+	payload = get_pokemons()
+	return render(request, 'pope.html', payload)
+
+def pokemon(request):
+	payload = get_card()
+	return render(request, 'card.html', {'d': payload})
 
 
 def db(request):
